8de374d0e4c893124d5566582070198a875e0ab0
hvsonn <hvson2003@gmail.com>
2024-04-03 21:52:15 +0700

    update temp

--- app/src/main/java/com/example/android_ck/DBHelper.java
+++ app/src/main/java/com/example/android_ck/DBHelper.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -84,9 +85,6 @@ public class DBHelper extends SQLiteOpenHelper {
         }
         cursor.close();
     }
-
-
-
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // Drop các bảng cũ nếu tồn tại
@@ -99,5 +97,38 @@ public class DBHelper extends SQLiteOpenHelper {
 
         // Tạo lại các bảng mới
         onCreate(db);
+    }
+    public void themDanhSachYeuThich(String tentaikhoan, String maphim){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("tentaikhoan", tentaikhoan);
+        cv.put("maphim", maphim);
+        long result = db.insert("danhsachyeuthich", null, cv);
+        if(result==-1){
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(context, "Added successfully!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void xoaDanhSachYeuThich(Integer id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        long result = db.delete("danhsachyeuthich", "iddansach=?", new String[]{String.valueOf(id)});
+        if(result == -1){
+            Toast.makeText(context, "Failed to delete", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(context, "Deleted Successfully", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public Cursor layDuLieuBangDSYT(){
+//        String query = "SELECT * FROM danhsachyeuthich dsyt INNER JOIN phim p WHERE dsyt.maphim=p.maphim";
+        String query = "SELECT * FROM danhsachyeuthich";
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = null;
+        if(db!=null){
+            cursor = db.rawQuery(query, null);
+        }
+        return cursor;
     }
 }
