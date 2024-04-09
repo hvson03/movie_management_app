b6d1558522165e89002282c2561c7d8c7b47d65d
NguyenVanDuong03 <19.nguyenvanduong@gmail.com>
2024-04-09 10:40:06 +0700

    Update animate IntroActivity

--- app/src/main/java/com/example/android_ck/IntroActivity.java
+++ app/src/main/java/com/example/android_ck/IntroActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.android_ck.khachhang.khachhang_dangnhap;
 
 public class IntroActivity extends AppCompatActivity {
+    TextView textView4, textView32;
     ImageButton btn_batdau;
 
     @Override
@@ -18,6 +20,19 @@ public class IntroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
         btn_batdau = findViewById(R.id.btn_batdau);
+        textView4 = findViewById(R.id.textView4);
+        textView32 = findViewById(R.id.textView32);
+
+        // Thiết lập alpha của các thành phần thành 0 (không hiển thị)
+        btn_batdau.setAlpha(0f);
+        textView4.setAlpha(0f);
+        textView32.setAlpha(0f);
+
+        // Sử dụng animate để thay đổi alpha từ 0 (không hiển thị) thành 1 (hiển thị đầy đủ)
+        btn_batdau.animate().alpha(1f).setDuration(3000).setStartDelay(1500);
+        textView4.animate().alpha(1f).setDuration(3000).setStartDelay(1000);
+        textView32.animate().alpha(1f).setDuration(3000).setStartDelay(1000);
+
         btn_batdau.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
